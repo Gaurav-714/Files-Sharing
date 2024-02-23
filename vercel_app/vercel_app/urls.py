4eabf387805f029a246fe6b50926e1bf7f1aff2e
Gaurav-714 <gauravnagose714@gmail.com>
2024-02-23 12:19:52 +0530

    updated

---
+++ vercel_app/vercel_app/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from example.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('download/<uid>', download),
+    path('file-upload/', HandleFileUplaod.as_view()),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
