bcec9bc20ee02b71f34eead2fec9d6ef8869f01b
Gaurav-714 <gauravnagose714@gmail.com>
2024-02-25 17:35:43 +0530

    Final

--- vercel_app/vercel_app/settings.py
+++ vercel_app/vercel_app/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_db',
+        'USER': 'harry',
+        'PASSWORD': 'DjDev714',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
