84b0b01d3d9e8ab857cd6f9b070759cc6dfccb61
Gaurav-714 <gauravnagose714@gmail.com>
2024-03-11 11:47:37 +0530

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -168,7 +168,7 @@ import os
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "public/static")
